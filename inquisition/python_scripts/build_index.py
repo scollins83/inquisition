ea7238c3487100e203d3f1fa5eb9a27a70629001
scollins83 <saracollins0508@gmail.com>
2020-09-10 22:36:54 -0500

    Working code for the squad_bert model in DeepPavlov.

--- inquisition/python_scripts/build_index.py
+++ inquisition/python_scripts/build_index.py
@@ -2,33 +2,64 @@ import inquisition as inq
 from pathlib import Path
 from tqdm import tqdm
 import whoosh.index
+import argparse
+import sys
 
+"""
 # Get the file list
-text_files = inq.get_file_names('../tests/artifacts/data/king_arthur/*.txt')
+text_path = '../tests/artifacts/data/king_arthur/*.txt'
+analyzer_type = 'Stemming'
+index_path = '/Users/saracollins/PycharmProjects/inquisition/inquisition/tests/artifacts/data/king_arthur_idx'
+index_name = 'arthur'
+"""
 
-# Create the blank schema
-schema = inq.create_title_and_text_schema(analyzer='Stemming')
 
-# Create the index
-idx = whoosh.index.create_in('/Users/saracollins/PycharmProjects/inquisition/inquisition/tests/artifacts/data/king_arthur_idx',
-                             schema=schema,
-                             indexname='arthur')
+def parse_args(args):
+    """
+    Returns arguments passed at the command line as a dict
+    :param args: Command line arguments
+    :return: args as a dict
+    """
+    parser = argparse.ArgumentParser(description='Create a whoosh index of text')
+    parser.add_argument('-t', '--text_path', help="Path where text to be indexed is stored.",
+                        required=True, dest='text_path')
+    parser.add_argument('-a', '--analyzer_type', help='Whoosh analyzer type to use for index',
+                        required=False, default='Stemming', dest='analyzer_type')
+    parser.add_argument('-ip', '--index_path', help='Path where the index should be stored',
+                        required=True, dest='index_path')
+    parser.add_argument('-in', '--index_name', help='Name assigned to the index',
+                        required=True, dest='index_name')
+    return vars(parser.parse_args(args))
 
-writer = idx.writer()
 
-for file in tqdm(text_files):
-    path = Path(file)
+def build_index():
+    args = parse_args(sys.argv[1:])
 
-    # Read in the info to be indexed
-    chapter_title = path.stem
-    with path.open('r') as f:
-        chapter_text = f.read()
+    text_files = inq.get_file_names(args['text_path'])
+    schema = inq.create_title_and_text_schema(analyzer=args['analyzer_type'])
 
-    # Add info to index
-    writer.update_document(
-        title=chapter_title,
-        text=chapter_text,
-    )
+    idx = whoosh.index.create_in(args['index_path'],
+                                 schema=schema,
+                                 indexname=args['index_name'])
 
-# Commit the index build
-writer.commit()
+    writer = idx.writer()
+
+    for file in tqdm(text_files):
+        path = Path(file)
+
+        chapter_title = path.stem
+        with path.open('r') as f:
+            chapter_text = f.read()
+
+        writer.update_document(
+            title=chapter_title,
+            text=chapter_text,
+        )
+
+    writer.commit()
+
+    return 1
+
+
+if __name__ == '__main__':
+    build_index()
